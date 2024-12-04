6150737cd6995b20be3a0319763f5cb37f091995
Manoj <manoj@cookiex.io>
2024-12-04 20:11:06 +0100

    fix: include logo in plugin package

--- resources/admin/js/components/Headers/AdminHeader.tsx
+++ resources/admin/js/components/Headers/AdminHeader.tsx
@@ -5,11 +5,7 @@ import { Image } from '@mantine/core';
 export function AdminHeader() {
 	return (
 		<header className="cookiex-admin-header">
-			<Image
-				w="180"
-				src="https://cdn.cookiex.io/brand/logo.svg"
-				alt="Cookiex logo"
-			/>
+			<Image w="180" src="static/logo.svg" alt="Cookiex logo" />
 		</header>
 	);
 }
