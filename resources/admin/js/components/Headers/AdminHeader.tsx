68699be01b2511d2dccf7c2167c2260bd6d41bf6
Manoj <manoj@cookiex.io>
2024-12-06 17:53:23 +0100

    fix: link the cookiex logo correctly

--- resources/admin/js/components/Headers/AdminHeader.tsx
+++ resources/admin/js/components/Headers/AdminHeader.tsx
@@ -5,7 +5,11 @@ import { Image } from '@mantine/core';
 export function AdminHeader() {
 	return (
 		<header className="cookiex-admin-header">
-			<Image w="180" src="static/logo.svg" alt="Cookiex logo" />
+			<Image
+				w="180"
+				src="/wp-content/plugins/cookiex-consent-management-platform/dist/static/logo.svg"
+				alt="Cookiex logo"
+			/>
 		</header>
 	);
 }
