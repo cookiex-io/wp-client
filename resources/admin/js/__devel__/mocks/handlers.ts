07b4ef9b3ff720ffb90a2c209c8372cdf135c092
Manoj <=>
2025-03-10 13:42:02 +0000

    fix: added mock apis

--- resources/admin/js/__devel__/mocks/handlers.ts
+++ resources/admin/js/__devel__/mocks/handlers.ts
@@ -169,8 +169,26 @@ export const handlers = [
 	}),
 
 	http.post('/cookiex/v1/save-settings', () => {
-		return HttpResponse.json('Settings saved successfully', {
-			status: 200,
+		return HttpResponse.json({
+			status: 'success',
+			message: 'Settings retrieved successfully',
+			data: {
+				domainId: '67cd617dc09b17267d62b35b',
+				language: 'en',
+				autoBlockCookies: false,
+				gtmEnabled: false,
+				gtmId: '',
+				cookiePreference: [],
+				serverCountry: 'in',
+				languagesAvailable: {
+					en: 'English',
+					fr: 'Fran\u00e7ais',
+					es: 'Espa\u00f1ol',
+					pt: 'Portugu\u00eas',
+					ar: '\u0627\u0644\u0639\u0631\u0628\u064a\u0629',
+				},
+				theme: '{"regulation":{"label":"GDPR","value":"gdpr","description":"The selected opt-in banner supports GDPR (EU & UK), LGPD (Brazil), PIPEDA (Canada), Law 25 (Quebec), POPIA (South Africa), nFADP (Switzerland), Privacy Act (Australia), PDPL (Saudi Arabia), PDPL (Argentina), PDPL (Andorra), DPA (Faroe Island), DPDP (India)"},"language":"en","layout":"Box","alignment":"leftBottomPopUp","theme":{"background":"#000","type":"Dark","buttonStyle":"Mixed","textColor":"#fff","highlight":"gray","buttonCustomizeBackGround":"#fff","buttonCustomizeTextColor":"#000","buttonCustomizeVariant":"filled","buttonCustomizeBorder":"#fff","buttonRejectBackGround":"#fff","buttonRejectTextColor":"#000","buttonRejectVariant":"filled","buttonRejectBorder":"#fff","buttonAcceptBackGround":"#fff","buttonAcceptTextColor":"#000","buttonAcceptVariant":"filled","buttonAcceptBorder":"#fff","consentOptions":[{"checked":true,"label":"Necessary"},{"checked":false,"label":"Preferences"},{"checked":false,"label":"Statistics"},{"checked":false,"label":"Marketing"},{"checked":false,"label":"Unclassified"}],"bannerPrefBg":"#fff","bannerPrefTxtClr":"#000","bannerPrefBtnAllowSelBackGround":"#0078b4","bannerPrefBtnAllowSelTextColor":"#fff","bannerPrefBtnAllowSelVariant":"filled","bannerPrefBtnAllowSelBorder":"#0078b4","bannerPrefBtnRejBackGround":"#fff","bannerPrefBtnRejTextColor":"#0078b4","bannerPrefBtnRejVariant":"filled","bannerPrefBtnRejBorder":"#0078b4","bannerPrefBtnAcptAllBackGround":"#0078b4","bannerPrefBtnAcptAllTextColor":"#fff","bannerPrefBtnAcptAllVariant":"filled","bannerPrefBtnAcptAllBorder":"#0078b4","bannerPreflanguageDd":"#000","bannerReconsentBg":"#fff","bannerReconsentTextColor":"#000","bannerReconsntBtnWithDrawBackGround":"#fff","bannerReconsntBtnWithDrawTextColor":"#0078b4","bannerReconsntBtnWithDrawVariant":"filled","bannerReconsntBtnWithDrawBorder":"#0078b4","bannerReconsntBtnUpdtCnsntBackGround":"#0078b4","bannerReconsntBtnUpdtCnsntTextColor":"#fff","bannerReconsntBtnUpdtCnsntVariant":"filled","bannerReconsntBtnUpdtCnsntBorder":"#0078b4"},"isLogoDisplay":true,"logo":"","method":"","type":"Dark","additionalDetailsConfig":{"isPreferences":false,"isStatistics":false,"isMarketing":false,"isDisplayCloseIcon":false},"baseUrl":"13.234.32.75","bannerContent":{"title":"We value your privacy","description":"We use cookies to enhance your browsing experience,serve personalized ads or content and analyze your traffic By clicking \'Accept\', you consent to our use of cookies.","options":{"Necessary":"Necessary","Preferences":"Preferences","Statistics":"Statistics","Marketing":"Marketing","Unclassified":"Unclassified"},"CookieConsent":"Cookie Consent","ConsentID":"Consent ID","ConsentGiven":"You have given consent for the use of","Timestamp":"Timestamp","Cookies":"Cookies","descriptions":{"Necessary":"These cookies are essential for the website to function properly. They enable basic functions like page navigation and access to secure areas of the website.","Preferences":"Preference cookies enable a website to remember information that changes the way the website behaves or looks, like your preferred language or the region you are in.","Statistics":"Statistic cookies help website owners to understand how visitors interact with websites by collecting and reporting information anonymously.","Marketing":"Marketing cookies are used to track visitors across websites. The intention is to display ads that are relevant and engaging for the individual user and thereby more valuable for publishers and third party advertisers.","Unclassified":"Unclassified cookies are those that we are currently working on categorizing, in collaboration with the providers of each individual cookie."},"Accept":"Accept All","AllowSelection":"Allow Selection","Reject":"Reject All","Customize":"Customize","WithdrawConsent":"Withdraw Consent","UpdateConsent":"Update Consent","modalTitle":"Cookie Consent","modalDescription":"Select the categories of cookies you consent to use.","savePreferences":"Save Preferences","alwaysActive":"Always Active","active":"Active","inactive":"Inactive","CookieName":"Cookie Name","CookieDescription":"Cookie Description","MaxStorageDuration":"Max Storage Duration","Type":"Type","Ok":"Ok","modalSubDescription":"Your opt-out settings for this website have been respected since we detected a Global Privacy Control signal from your browser and, therefore, you cannot change this setting.","Cancel":"Cancel","NotSellData":"Do Not Sell My Personal Information","policyUrl":""}}',
+			},
 		});
 	}),
 
@@ -206,5 +224,70 @@ export const handlers = [
 			},
 			{ status: 200 }
 		);
+	}),
+
+	http.get('/cookiex/v1/analytics', () => {
+		return HttpResponse.json({
+			data: {
+				status: 'success',
+				message: 'Analytics data retrieved successfully.',
+				data: {
+					averageTimeToConsent: null,
+					leastAcceptedCookie: 'Marketing',
+					mostAcceptedCookie: 'Unclassified',
+					totalOptOuts: 0,
+					totalOptIns: 0,
+					chartData: [],
+				},
+			},
+			headers: [],
+			status: 200,
+		});
+	}),
+
+	http.get('/cookiex/v1/cookie-scan', () => {
+		return HttpResponse.json({
+			data: {
+				status: 'success',
+				message: 'Cookie scan data retrieved successfully.',
+				data: {
+					domainId: '67cd617dc09b17267d62b35b',
+					domainUrl: 'https://13.234.32.75',
+					frequency: 'Monthly',
+					status: 'Completed',
+					history: [
+						{
+							id: '67cd61c5764e0533d4876927',
+							status: 'Completed',
+							pages: 5,
+							cookiesCount: 2,
+							scannedOn: '2025-03-09T09:39:17.055Z',
+						},
+						{
+							id: '67cd6185764e0533d4876926',
+							status: 'Completed',
+							pages: 1,
+							cookiesCount: 2,
+							scannedOn: '2025-03-09T09:38:12.901Z',
+						},
+					],
+					latestScanResult: {
+						server: 'IN',
+						status: 'Completed',
+						pages: 5,
+						necessaryCookies: 1,
+						preferenceCookies: 0,
+						statisticsCookies: 0,
+						marketingCookies: 0,
+						unclassifiedCookies: 1,
+						cookiesCount: 2,
+						scannedOn: '2025-03-09T09:39:17.055Z',
+					},
+					nextScan: '2025-04-08T09:39:17.460Z',
+				},
+			},
+			headers: [],
+			status: 200,
+		});
 	}),
 ];
